37d5948198c977aa55ad334932645e0e81dbaab2
Zsolt <zsolt.gombocz00@gmail.com>
2019-05-03 22:19:17 +0200

    One ajax call for registration data

--- assets/js/user_registration.js
+++ assets/js/user_registration.js
@@ -1,14 +1,19 @@
 function load_data(id) {
     $.ajax({
-        url: "http://www.modminers.hu/request/registration/intro/" + id,
+        url: "http://www.modminers.hu/request/registration/info/" + id,
         method: "POST",
-        data:{data:true},
-        success: function(data) {
-            $(".result").html(data);
-        }     
-    });
+        data: {data: true},
+        success: function (data) {
+            var obj = JSON.parse(data);
 
-    $.ajax({
+            $(".result").html(obj.introduction);
+            $(".regusername").html(obj.username);
+            $(".reg_date").html(obj.reg_date);
+        }
+    });
+}
+
+    /*$.ajax({
         url: "http://www.modminers.hu/request/registration/intro/username/" + id,
         method: "POST",
         data:{data:true},
@@ -16,4 +21,4 @@ function load_data(id) {
             $(".regusername").html(data);
         }     
     });
-}
+}*/
