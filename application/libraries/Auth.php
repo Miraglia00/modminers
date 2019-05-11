32e5af72c265ac809d0cc340db61b2609c0ba63a
Zsolt <zsolt.gombocz00@gmail.com>
2019-05-11 19:07:59 +0200

    siteURL function

--- application/libraries/Auth.php
+++ application/libraries/Auth.php
@@ -14,4 +14,10 @@ class Auth
         $token = hash('sha256', $user_code.$generated);
     }
 
+    function siteURL() {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $domainName = $_SERVER['HTTP_HOST'] . '/';
+        return $protocol . $domainName;
+    }
+
 }
